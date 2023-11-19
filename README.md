c5b3d7e0a51055975f0ede4444d3cf40e74200c7
Gerard Cathalifaud <gerard.an@hotmail.com>
2023-11-19 17:14:37 -0300

    distribuyendo y modularizando las funciones en archivos

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # tarea-3-logaritmos
  tarea 3 del curso Diseño y análisis de algoritmo
+
+
+# Compilar con dependencias:
+Tener en cuenta que compilar archivos con otras dependencias deben ser enunciadas al momento de compilar.
+-   -I.\ indica que debe incluir la carpeta actual como parte del fichero de archivos a poder incluir
+-   -o "ejecutable" indica el nombre del archivo de salida.
+-   Luego se agregan los archivos a compilar, el primero es el principal y los otros las dependencias.
+
+### Para compilar el algoritmo de divide y conquista con el archivo recursos.
+```
+g++ -Wall -I.\ -o divide-and-conquer.exe divide-and-conquer.cpp resources.cpp
+```
+
+También se puede hacer paso a paso:
+```
+g++ -Wall -c resources.cpp
+g++ -I.\ -Wall -c divide-and-conquer.cpp
+g++ -Wall -o divide-and-conquer.exe divide-and-conquer.o resources.o
+```
---
+++ resources.cpp
@@ -0,0 +1,52 @@
+#include "resources.h"
+#include "cmath"
+#include "cfloat"
+
+using namespace std;
+
+/* Following two functions are needed for library function qsort(). 
+Refer: http://www.cplusplus.com/reference/clibrary/cstdlib/qsort/ */
+
+// Needed to sort array of points 
+// according to X coordinate 
+int compareX(const void* a, const void* b) 
+{ 
+	Point *p1 = (Point *)a, *p2 = (Point *)b; 
+	return (p1->x - p2->x); 
+} 
+
+// Needed to sort array of points according to Y coordinate 
+int compareY(const void* a, const void* b) 
+{ 
+	Point *p1 = (Point *)a, *p2 = (Point *)b; 
+	return (p1->y - p2->y); 
+} 
+
+// A utility function to find the 
+// distance between two points 
+float dist(Point p1, Point p2) 
+{ 
+	return sqrt( (p1.x - p2.x)*(p1.x - p2.x) + 
+				(p1.y - p2.y)*(p1.y - p2.y) 
+			); 
+} 
+
+// A Brute Force method to return the 
+// smallest distance between two points 
+// in P[] of size n 
+float bruteForce(Point P[], int n) 
+{ 
+	float min = FLT_MAX; 
+	for (int i = 0; i < n; ++i) 
+		for (int j = i+1; j < n; ++j) 
+			if (dist(P[i], P[j]) < min) 
+				min = dist(P[i], P[j]); 
+	return min; 
+} 
+
+// A utility function to find 
+// minimum of two float values 
+float min(float x, float y) 
+{ 
+	return (x < y)? x : y; 
+} 
